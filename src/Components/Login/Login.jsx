59e42b5ec3d952e0759ba7bb621796669f3ca3e6
Zarif <zarifraja19@gmail.com>
2023-05-27 22:15:40 +0600

    added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { Link } from "react-router-dom";
 import Title from "../../Title/Title";
 
-
+import img1 from '../../assets/login/Frame (1).png'
+import img2 from '../../assets/login/Frame (2).png'
+import img3 from '../../assets/login/Group (1).png'
+import img4 from '../../assets/login/Group.png'
+import img5 from '../../assets/login/Rectangle 1.png'
 
 const Login = () => {
     Title('DOC | LOG IN')
@@ -8,14 +14,9 @@ const Login = () => {
     <div>
         
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+           <img src={img1} alt="" />
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
@@ -47,6 +48,7 @@ const Login = () => {
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
+              <div><Link to='/'>HOME</Link></div>
             </div>
           </div>
         </div>
--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../Components/Home/Navbar/Navbar";
 
 
 const Main = () => {
+     const location = useLocation();
+     const noHeaderFooter =
+       location.pathname.includes("login") ||
+       location.pathname.includes("signUp");
     return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
+      <div>
+        {noHeaderFooter || <Navbar></Navbar>}
+        <Outlet></Outlet>
+      </div>
     );
 };
 
